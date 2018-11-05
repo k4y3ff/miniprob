01ad6747d5a0e10b8b920fa9d7bbb0420dcf390b
kf Fellows <k@therinefello.ws>
2018-11-04 22:23:06 -0800

    2.3 Flipping coins

--- src/miniprob/core.clj
+++ src/miniprob/core.clj
@@ -2,6 +2,12 @@
   (:gen-class)
   (:refer-clojure :exclude [eval]))
 
+(defn flip []
+  """
+  Evaluates to 0 with probability 0.5 and 1 with probability 0.5.
+  """
+  (if (<= (rand) 0.5) 0 1))
+
 (defn eval [exp]
   """
   Evaluates the given expression.
@@ -12,7 +18,11 @@
     (= '- exp)        clojure.core/-
     (seq? exp)
       (let [[operator & args] exp]
-        (apply (eval operator) (map #(eval %) args)))
+        (cond
+          (= 'flip operator) (flip)
+          :else
+            (apply (eval operator)
+                   (map #(eval %) args))))
     :else
       (->> exp
            (str "THERE WAS AN ATTEMPT: ")
--- test/miniprob/core_test.clj
+++ test/miniprob/core_test.clj
@@ -20,3 +20,19 @@
 (deftest primitive-procedures-test
   (testing "an expression with both - and + evaluates as expected"
     (is (= (eval '(+ 1 (- 3 2))) 2))))
+
+(deftest flip-test
+  (testing "flip returns 0 50% of the time and 1 50% of the time"
+    (let [results (repeatedly 100 #(eval '(flip)))
+          zero-count (count (filter zero? results))
+          one-count (count (filter #(= % 1) results))]
+      (is (= (+ zero-count one-count) 100))
+      (is (and (>= zero-count 40) (<= zero-count 60)))
+      (is (and (>= one-count 40) (<= one-count 60))))))
+
+(deftest flipping-coins-test
+  (testing "flip evaluates correctly when part of an expression with other operators"
+    (let [results (repeatedly 100 #(eval '(+ (flip) 2)))]
+      (is (empty? (filter #(not (contains? #{2 3} %)) results))))
+    (let [results (repeatedly 100 #(eval '(+ (flip) (flip))))]
+      (is (empty? (filter #(not (contains? #{0 1 2} %)) results))))))
