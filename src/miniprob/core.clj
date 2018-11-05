e014fe279eb42e89a5cf109459a984569e5dc654
kf Fellows <k@therinefello.ws>
2018-11-04 23:31:41 -0800

    2.4 Program traces

--- src/miniprob/core.clj
+++ src/miniprob/core.clj
@@ -2,29 +2,45 @@
   (:gen-class)
   (:refer-clojure :exclude [eval]))
 
-(defn flip []
-  """
-  Evaluates to 0 with probability 0.5 and 1 with probability 0.5.
-  """
-  (if (<= (rand) 0.5) 0 1))
+(defn flip
+  ([]
+    "Evaluates to 0 with probability 0.5 and 1 with probability 0.5."
+    (if (<= (rand) 0.5) 0 1))
+  ([tag trace]
+    """
+    If the tag is a key in the trace, returns the corresponding value from the
+    trace. Otherwise, evaluates to 0 with a probability 0.5 and 1 with
+    probability 0.5.
+    """
+    (let [t (get trace tag)]
+      (if (nil? t)
+        (flip)
+        t))))
 
-(defn eval [exp]
-  """
-  Evaluates the given expression.
-  """
-  (cond
-    (number? exp)     exp
-    (= '+ exp)        clojure.core/+
-    (= '- exp)        clojure.core/-
-    (seq? exp)
-      (let [[operator & args] exp]
-        (cond
-          (= 'flip operator) (flip)
-          :else
-            (apply (eval operator)
-                   (map #(eval %) args))))
-    :else
-      (->> exp
-           (str "THERE WAS AN ATTEMPT: ")
-           (Exception.)
-           throw)))
+(defn eval
+  ([exp]
+    """
+    Evaluates the given expression.
+    """
+    (eval exp {}))
+  ([exp trace]
+    """
+    Evaluates the given expression, overriding flip call sites using the given
+    trace when indicated.
+    """
+    (cond
+      (number? exp)     exp
+      (= '+ exp)        clojure.core/+
+      (= '- exp)        clojure.core/-
+      (seq? exp)
+        (let [[operator & args] exp]
+          (cond
+            (= 'flip operator) (flip (first args) trace)
+            :else
+              (apply (eval operator trace)
+                     (map #(eval % trace) args))))
+      :else
+        (->> exp
+             (str "THERE WAS AN ATTEMPT: ")
+             (Exception.)
+             throw))))
--- test/miniprob/core_test.clj
+++ test/miniprob/core_test.clj
@@ -21,7 +21,7 @@
   (testing "an expression with both - and + evaluates as expected"
     (is (= (eval '(+ 1 (- 3 2))) 2))))
 
-(deftest flip-test
+(deftest flip-no-args-test
   (testing "flip returns 0 50% of the time and 1 50% of the time"
     (let [results (repeatedly 100 #(eval '(flip)))
           zero-count (count (filter zero? results))
@@ -36,3 +36,21 @@
       (is (empty? (filter #(not (contains? #{2 3} %)) results))))
     (let [results (repeatedly 100 #(eval '(+ (flip) (flip))))]
       (is (empty? (filter #(not (contains? #{0 1 2} %)) results))))))
+
+(deftest flip-args-exist-test
+  (testing "when eval is provided with a trace and flip is provided with a call-site tag, flip
+      returns the expected value from the trace"
+    (is (= (eval '(flip :tag) {:tag 0}) 0))
+    (is (= (eval '(+ (flip :a) (flip :b)) {:a 2 :b 3}) 5))))
+
+(deftest flip-args-do-not-exist-test
+  (testing "when eval is provided with a trace and flip is provided with a call-site tag, flip
+      returns either 0 or 1 if the tag does not exist in the trace"
+    (let [results (repeatedly 100 #(eval '(flip :tag) {}))]
+      (is (zero? (count (filter #(and (not (zero? %)) (not= 1 %)) results)))))))
+
+(deftest flip-args-no-args-test
+  (testing "when eval is provided with a trace, and some but not all flips are provided with a
+      call-site tag, flip returns expected trace values when indicated and either 0 or 1 otherwise"
+    (let [results (repeatedly 100 #(eval '(+ (flip) (flip :a) (flip :b)) {:a 0 :b 1}))]
+      (is (zero? (count (filter #(and (not= 1 %) (not= 2 %)) results)))))))
