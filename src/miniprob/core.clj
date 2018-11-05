26d8a4546e1b891f8c1b5192b1ad6d2f0a4ef6b4
kf Fellows <k@therinefello.ws>
2018-11-04 23:41:33 -0800

    2.5 Functions

--- src/miniprob/core.clj
+++ src/miniprob/core.clj
@@ -1,6 +1,9 @@
 (ns miniprob.core
-  (:gen-class)
-  (:refer-clojure :exclude [eval]))
+  (:refer-clojure :exclude [apply eval]))
+
+(declare apply)
+
+(defrecord Procedure [params body])
 
 (defn flip
   ([]
@@ -22,25 +25,44 @@
     """
     Evaluates the given expression.
     """
-    (eval exp {}))
+    (eval exp {} {}))
   ([exp trace]
     """
     Evaluates the given expression, overriding flip call sites using the given
     trace when indicated.
     """
+    (eval exp trace {}))
+  ([exp trace env]
+    """
+    Evaluates the given expression, overriding flip call sites using the given
+    trace when indicated and using the given environment env to evaluate
+    variables in anonymous functions.
+    """
     (cond
       (number? exp)     exp
       (= '+ exp)        clojure.core/+
       (= '- exp)        clojure.core/-
+      (symbol? exp)     (env exp)
       (seq? exp)
         (let [[operator & args] exp]
           (cond
-            (= 'flip operator) (flip (first args) trace)
+            (= 'flip operator)
+              (flip (first args) trace)
+            (= 'fn operator)
+              (let [[params body] args]
+                (Procedure. params body))
             :else
-              (apply (eval operator trace)
-                     (map #(eval % trace) args))))
-      :else
-        (->> exp
-             (str "THERE WAS AN ATTEMPT: ")
-             (Exception.)
-             throw))))
+              (apply (eval operator trace env)
+                     trace
+                     (map #(eval % trace env) args)))))))
+
+(defn apply [procedure trace args]
+  """
+  Given a procedure--which can be either a Clojure function or a Procedure--
+  along with a trace and arguments for the procedure, applies the procedure to
+  the arguments, overriding flip call sites using the given trace when
+  indicated.
+  """
+ (if (= (type procedure) Procedure)
+   (eval (:body procedure) trace (zipmap (:params procedure) args))
+   (clojure.core/apply procedure args)))
--- test/miniprob/core_test.clj
+++ test/miniprob/core_test.clj
@@ -31,26 +31,31 @@
       (is (and (>= one-count 40) (<= one-count 60))))))
 
 (deftest flipping-coins-test
-  (testing "flip evaluates correctly when part of an expression with other operators"
+  (testing "flip evaluates correctly when part of an expression with other
+      operators"
     (let [results (repeatedly 100 #(eval '(+ (flip) 2)))]
       (is (empty? (filter #(not (contains? #{2 3} %)) results))))
     (let [results (repeatedly 100 #(eval '(+ (flip) (flip))))]
       (is (empty? (filter #(not (contains? #{0 1 2} %)) results))))))
 
 (deftest flip-args-exist-test
-  (testing "when eval is provided with a trace and flip is provided with a call-site tag, flip
-      returns the expected value from the trace"
+  (testing "when eval is provided with a trace and flip is provided with a
+      call-site tag, flip returns the expected value from the trace"
     (is (= (eval '(flip :tag) {:tag 0}) 0))
     (is (= (eval '(+ (flip :a) (flip :b)) {:a 2 :b 3}) 5))))
 
 (deftest flip-args-do-not-exist-test
-  (testing "when eval is provided with a trace and flip is provided with a call-site tag, flip
-      returns either 0 or 1 if the tag does not exist in the trace"
+  (testing "when eval is provided with a trace and flip is provided with a
+      call-site tag, flip returns either 0 or 1 if the tag does not exist in
+      the trace"
     (let [results (repeatedly 100 #(eval '(flip :tag) {}))]
       (is (zero? (count (filter #(and (not (zero? %)) (not= 1 %)) results)))))))
 
 (deftest flip-args-no-args-test
-  (testing "when eval is provided with a trace, and some but not all flips are provided with a
-      call-site tag, flip returns expected trace values when indicated and either 0 or 1 otherwise"
-    (let [results (repeatedly 100 #(eval '(+ (flip) (flip :a) (flip :b)) {:a 0 :b 1}))]
+  (testing "when eval is provided with a trace, and some but not all flips are
+      provided with a call-site tag, flip returns expected trace values when
+      indicated and either 0 or 1 otherwise"
+    (let [results (repeatedly 100
+                              #(eval '(+ (flip) (flip :a) (flip :b))
+                                      {:a 0 :b 1}))]
       (is (zero? (count (filter #(and (not= 1 %) (not= 2 %)) results)))))))
